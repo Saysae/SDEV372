3e7ee1c145fe983ee00c103fa1392c980643dd6a
Admin <luong.patrick@student.greenriver.edu>
2023-01-10 10:05:11 -0800

    Revert "Added getters and setters along with a toString"
    This reverts commit 677075e9

--- src/making_changes/Point.java
+++ src/making_changes/Point.java
@@ -1,7 +1,5 @@
 package making_changes;
 
-import java.util.Objects;
-
 public class Point
 {
     private double x;
@@ -13,44 +11,5 @@ public class Point
         this.y = y;
     }
 
-    public double getX() {
-        return x;
-    }
 
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Point point = (Point) o;
-        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
-
-    @Override
-    public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
-    }
 }
